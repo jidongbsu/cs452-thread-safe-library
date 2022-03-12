19be382549b2bd351e61afb8c55a200f2e7bbd0a
jidongbsu <jidongxiao@boisestate.edu>
2022-03-12 14:40:08 -0700

    adding empty functions into .c and adding comments

--- wrapper-library/ThreadsafeBoundedList.c
+++ wrapper-library/ThreadsafeBoundedList.c
@@ -14,3 +14,30 @@ struct tsb_list {
 };
 
 /* write all your functions below */
+
+/* this function returns a pointer to the allocated tsb list */
+struct tsb_list * tsb_createList(int (*equals)(const void *, const void *), char * (*toString)(const void *), void (*freeObject)(void *), int capacity){
+	return NULL;
+}
+
+void tsb_freeList(struct tsb_list * list){
+}
+
+void tsb_addAtFront(struct tsb_list * list, NodePtr node){
+}
+
+void tsb_addAtRear(struct tsb_list * list, NodePtr node){
+}
+
+/* this function returns a pointer to the node that was removed */
+NodePtr tsb_removeFront(struct tsb_list * list){
+	return NULL;
+}
+
+/* this function returns a pointer to the node that was removed */
+NodePtr tsb_removeRear(struct tsb_list * list){
+	return NULL;
+}
+
+void tsb_finishUp(struct tsb_list * list){
+}
